9a489c3021069789ff805d274d2eafd32839474a
Aleksander Püüa <aleksander.puua@voco.ee>
2024-11-25 12:12:07 +0200

    Praks 7 - Get-Random kasutamine

---
+++ pr7.ps1
@@ -0,0 +1,9 @@
+﻿$groups = @('Red', 'Green', 'Yellow', 'Blue')
+$StudentGroups = @()
+
+for($i = 1; $i -le 20; $i++){
+    $group = $groups | Get-Random
+    $studentGroups += [PSCustomObject]@{'RollNumber'=$i; 'Group'=$group}
+}
+
+$studentGroups
